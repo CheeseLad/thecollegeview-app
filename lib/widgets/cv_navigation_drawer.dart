a6bd98c227f38df4f620c804800c20b75de3e353
Jake Farrell <jake_farrell@outlook.com>
2026-08-12 23:56:17 +0100

    generate app icons, add whatsapp link, always align social icons to bottom

--- lib/widgets/cv_navigation_drawer.dart
+++ lib/widgets/cv_navigation_drawer.dart
@@ -18,103 +18,114 @@ class CVNavigationDrawer extends StatelessWidget {
     final articleProvider = Provider.of<ArticleProvider>(context);
 
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/logo.png',
-                    height: 135,
-                    width: 300,
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          'assets/logo.png',
+                          height: 135,
+                          width: 300,
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+
+                ListTile(
+                  title: const Text('All Articles'),
+                  onTap: () {
+                    articleProvider.clearFilters();
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const ArticlesScreen(categoryName: "All Articles"),
+                      ),
+                    );
+                  },
+                ),
+
+                ListTile(
+                  title: const Text('Saved Articles'),
+                  leading: const Icon(Icons.bookmark),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SavedArticlesScreen(),
+                      ),
+                    );
+                  },
+                ),
+
+                ...articleProvider.categories.map((category) {
+                  return _buildCategoryTile(context, category);
+                }),
+
+                ListTile(
+                  title: const Text('About'),
+                  leading: const Icon(Icons.info),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AboutScreen(),
+                      ),
+                    );
+                  },
+                ),
+
+                ListTile(
+                  title: const Text('Contact'),
+                  leading: const Icon(Icons.contact_mail),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ContactScreen(),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
           ),
-          ListTile(
-            title: const Text('All Articles'),
-            onTap: () {
-              articleProvider.clearFilters();
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ArticlesScreen(categoryName: "All Articles"),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Saved Articles'),
-            leading: const Icon(Icons.bookmark),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SavedArticlesScreen(),
-                ),
-              );
-            },
-          ),
-          ...articleProvider.categories.map((category) {
-            return _buildCategoryTile(context, category);
-          }),
-          /*ListTile(
-            title: const Text('Submit Article'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ArticleSubmissionPage(),
-                ),
-              );
-            },
-          ),*/
-          ListTile(
-            title: const Text('About'),
-            leading: const Icon(Icons.info),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AboutScreen(),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Contact'),
-            leading: const Icon(Icons.contact_mail),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ContactScreen(),
-                ),
-              );
-            },
-          ),
-          const SizedBox(height: 10),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SocialMediaIcon(
+
+          // Always stays at the bottom
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SocialMediaIcon(
                   icon: FontAwesomeIcons.instagram,
-                  url: 'https://instagram.com/thecollegeview'),
-              SizedBox(width: 10),
-              SocialMediaIcon(
-                  icon: FontAwesomeIcons.link,
-                  url: 'https://thecollegeview.ie'),
-            ],
+                  url: 'https://instagram.com/thecollegeview',
+                ),
+                SizedBox(width: 10),
+                SocialMediaIcon(
+                  icon: FontAwesomeIcons.whatsapp,
+                  url: 'https://chat.whatsapp.com/C8Rwkvo7h7k4STz79ICfHV',
+                ),
+                SizedBox(width: 10),
+                SocialMediaIcon(
+                  icon: FontAwesomeIcons.globe,
+                  url: 'https://thecollegeview.ie',
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 10),
         ],
       ),
     );
